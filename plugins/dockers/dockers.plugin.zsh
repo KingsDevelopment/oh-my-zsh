19198259d703221d54de69a8fc0c86584894c5f8
Jurien Hamaker <jurienhamaker@gmail.com>
2018-01-25 14:34:17 +0100

    refractor

--- plugins/dockers/dockers.plugin.zsh
+++ plugins/dockers/dockers.plugin.zsh
@@ -1,15 +1,12 @@
-# locations
-alias p='cd /mnt/Programming/'
-alias sb='p; cd SuperBuddy'
-
-# dockers
+# Work
+## dockers
 alias docker_data='sb; cd Saitama; docker-compose -f dockerfiles/Loopback/Saitama/docker-compose.dev.yml up redis db search;'
 alias docker_saitama='sb; cd Saitama; docker-compose -f dockerfiles/Loopback/Saitama/docker-compose.dev.yml up loopback;'
 alias docker_sb='sb; cd SuperBuddy; docker-compose -f dockerfiles/Angular/superbuddy/docker-compose.dev.yml up;'
 alias docker_bo='sb; cd BackOffice; docker-compose -f dockerfiles/Angular/backoffice/docker-compose.dev.yml up;'
 alias docker_docs='sb; cd DevDocs; docker-compose -f dockerfiles/MkDocs/docker-compose.dev.yml up;'
 
-#hero association
+## hero association
 alias hero_saitama='sb; cd Hero-Association/Saitama; docker-compose -f dockerfiles/Loopback/Saitama/docker-compose.dev.yml up;'
 alias hero_genos='sb; cd Hero-Association/Genos; docker-compose -f dockerfiles/Loopback/Genos/docker-compose.dev.yml up;'
 alias hero_mumen='sb; cd Hero-Association/Mumen-Rider; docker-compose -f dockerfiles/Loopback/Mumen-Rider/docker-compose.dev.yml up;'
---
+++ plugins/locations/locations.plugin.zsh
@@ -0,0 +1,8 @@
+# shortcuts
+alias ohmyzsh='cd ~/.oh-my-zsh'
+
+# custom locations
+alias p='cd /mnt/Programming/'
+alias sb='p; cd SuperBuddy'
+alias king='p; cd Kings-Dev'
+
