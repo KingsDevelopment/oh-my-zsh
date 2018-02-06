f97e6978099b576e62c14252cbe1bad6781eae90
Jurien Hamaker <jurien@superbuddy.nl>
2018-02-06 13:50:09 +0100

    add sonic to dockers

--- plugins/dockers/dockers.plugin.zsh
+++ plugins/dockers/dockers.plugin.zsh
@@ -10,3 +10,5 @@ alias docker_docs='sb; cd DevDocs; docker-compose -f dockerfiles/MkDocs/docker-c
 alias hero_saitama='sb; cd Hero-Association/Saitama; docker-compose -f dockerfiles/Loopback/Saitama/docker-compose.dev.yml up;'
 alias hero_genos='sb; cd Hero-Association/Genos; docker-compose -f dockerfiles/Loopback/Genos/docker-compose.dev.yml up;'
 alias hero_mumen='sb; cd Hero-Association/Mumen-Rider; docker-compose -f dockerfiles/Loopback/Mumen-Rider/docker-compose.dev.yml up;'
+alias hero_sonic='sb; cd Hero-Association/Sonic; docker-compose -f dockerfiles/Loopback/Sonic/docker-compose.dev.yml up;'
+
