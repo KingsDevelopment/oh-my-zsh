c2b4544d65b680ec182fbeb96af8b797c826317a
Jurien Hamaker <jurien@superbuddy.nl>
2018-03-06 11:59:58 +0100

    add booter

--- plugins/dockers/dockers.plugin.zsh
+++ plugins/dockers/dockers.plugin.zsh
@@ -7,6 +7,7 @@ alias docker_bo='sb; cd BackOffice; docker-compose -f dockerfiles/Angular/backof
 alias docker_docs='sb; cd DevDocs; docker-compose -f dockerfiles/MkDocs/docker-compose.dev.yml up;'
 
 ## hero association
+alias hero_association='sb; cd Hero-Association/Booter; python boot.py;'
 alias hero_saitama='sb; cd Hero-Association/Saitama; docker-compose -f dockerfiles/Loopback/Saitama/docker-compose.dev.yml up;'
 alias hero_genos='sb; cd Hero-Association/Genos; docker-compose -f dockerfiles/Loopback/Genos/docker-compose.dev.yml up;'
 alias hero_mumen='sb; cd Hero-Association/Mumen-Rider; docker-compose -f dockerfiles/Loopback/Mumen-Rider/docker-compose.dev.yml up;'
