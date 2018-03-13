cf832114b68aebabdcdc397623fd7e6728db875d
Jurien Hamaker <jurien@superbuddy.nl>
2018-03-13 15:32:49 +0100

    sometimes I copy pasta things wrong

--- plugins/cleandockers/cleandockers.plugin.zsh
+++ plugins/cleandockers/cleandockers.plugin.zsh
@@ -2,7 +2,7 @@
 alias clean_containers='docker rm $(docker ps -a -q)';
 
 # Deletes all images
-alias clean_images='docker rmi $(docker images -a)';
+alias clean_images='docker rmi $(docker images -q)';
 
 # Does it both after each other. Cause u know. It's easier in once command.
 alias clean_dockers='clean_containers; clean_images';
